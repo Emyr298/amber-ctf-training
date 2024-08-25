f16caa77378e92ef393886f7a874ba20a78ffddf
Emyr298 <emi.ru298@gmail.com>
2024-08-25 15:45:01 +0700

    feat: change discord notification

--- amber/ticks.py
+++ amber/ticks.py
@@ -10,20 +10,31 @@ from utils.flags import generate_flag
 from config import GLOBAL
 
 from utils.flags import change_flag_async
-from utils.discord import notify_background
+from utils.discord import notify, notify_background
+
+def notify_challs():
+    msg = "**🚩🚩🚩 CTF has started 🚩🚩🚩**\n\n"
+    with Session(engine) as session:
+        for team in get_teams(session):
+            msg_team = f"**{team.name}**\n"
+            cnt_container = 0
+            for container in get_containers(session, team):
+                cnt_container += 1
+                msg_team += f"- {container.chall.name}: {container.team.host} {container.chall.port}\n"
+            if cnt_container == 0:
+                msg_team += "No Challs"
+            msg_team += "\n\n"
+            msg += msg_team
+    notify(msg)
 
 def change_flags():
     GLOBAL["CURRENT_TICK"] += 1
-    print("TICK {}".format(GLOBAL["CURRENT_TICK"]))
-    
-    notify_background("**Tick {} Started**".format(GLOBAL["CURRENT_TICK"]))
-    
+    notify_background("**⏰⏰ Tick {} ⏰⏰**".format(GLOBAL["CURRENT_TICK"]))
     with Session(engine) as session:
         for team in get_teams(session):
             for container in get_containers(session, team):
                 while True:
                     flag = generate_flag()
-                    print(flag)
                     try:
                         add_flag(session, container, flag, GLOBAL["CURRENT_TICK"])
                     except:
@@ -33,6 +44,7 @@ def change_flags():
         session.commit()
 
 def setup_ticks():
+    notify_challs()
     change_flags()
     scheduler = BackgroundScheduler()
     scheduler.add_job(change_flags, CronTrigger(minute="*/5"))
